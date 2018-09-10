071342c962da68d87731d0c584567f9909a36842
TheKewlStore <davis.835@wright.edu>
2018-09-10 18:42:04 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1.dir/main.cpp.o"
+  "../bin/Project1.pdb"
+  "../bin/Project1.exe"
+  "libProject1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
